59c26b728fe4bbae30d510ff62ac50c7572b8171
jono.carnie <jono.carnie@81d31cc1-63c2-b317-b071-6e3abfb8f55e>
2010-08-14 05:42:09 +0000

    Initial Build

---
+++ SCM Test/empty.c
@@ -0,0 +1,19 @@
+/*
+ *  empty.c
+ *  SCM Test
+ *
+ *  Created by Jonathan Carnie on 14/08/10.
+ *  Copyright 2010 Jonathan Carnie. All rights reserved.
+ *
+ */
+
+#include "empty.h"
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello world.");
+	return 0;
+}
+
+
